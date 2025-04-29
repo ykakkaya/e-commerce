152d78d3329884f493b5aaa54ea536dac5a7472b
ykakkaya <ykakkaya@hotmail.com>
2025-04-29 08:32:40 +0300

    user route middleware

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/',[HomeController::class,'index'])->name('home');
 //     return view('dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->prefix('user')->group(function () {
-  Route::get('/dashboard',[UserController::class,'index'])->name('user.dashboard');  
+Route::middleware(['auth','role:user'])->prefix('user')->group(function () {
+  Route::get('dashboard',[UserController::class,'index'])->name('user.dashboard');  
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
